5f536d291b427525ae4101199ed9841a3c492018
rvaabadilla <rvaabadilla@gmail.com>
2019-10-15 01:33:44 +0800

    Refactored triviaAPI and tourAPI to match newsAPI

--- pages/tourAPI.php
+++ pages/tourAPI.php
@@ -1,67 +1,64 @@
 <?php
 //TourAPI, for the virtual tour
     require 'globaldbconnection.php' ;
+    class Tour{
+        public $conn;
+        public $current_place = null ;
 
-    $conn = get_db_connection() ;
-    $query = "SELECT count(1) FROM Trivia" ;
-    $results = mysqli_query($conn, $query) ;
-    $row = mysql_fetch_array($result);
-    $current_place = null ;
-
-    function fetch_place_by_id($id){
-        global $current_place ;
-        $query = "SELECT * FROM Digitour WHERE id=" . $id ;
-        $current_place = mysql_fetch_array($result);
-    }
-
-    function fetch_place_by_name($name){
-        global $current_place ;
-        $query = "SELECT * FROM Digitour WHERE name='" . $name . "'" ;
-        $current_place = mysql_fetch_array($result);
-    }
-
-    function move($direction){ //use north, south, east, west
-        global $current_place ;
-        switch($direction){
-            case "north":
-                $current_place == fetch_place_by_id($current_place["adjacent_north_id"]) ;
-                break;
-            case "south":
-                $current_place == fetch_place_by_id($current_place["adjacent_south_id"]) ;
-                break;
-            case "west":
-                $current_place == fetch_place_by_id($current_place["adjacent_west_id"]) ;
-                break;
-            case "east":
-                $current_place == fetch_place_by_id($current_place["adjacent_east_id"]) ;
-                break;
-            default:
-                Print '<script>alert("Direction invalid!");</script>'; 
-
+        public function __construct()
+        {
+            $this->conn = get_db_connection();
         }
-    } 
-    function get_current_place_image(){
-        global $current_place ;
-        if($current_place == null){
-            Print '<script>alert("Place not set!");</script>';    
+        public function fetch_place_by_id($id){
+            $query = "SELECT * FROM Digitour WHERE id=" . $id ;
+            $current_place = mysql_fetch_array($result);
         }
-        return  $current_place["image_path"] ;
-    }
 
-    function get_current_place_name(){
-        global $current_place ;
-        if($current_place == null){
-            Print '<script>alert("Place not set!");</script>';    
+        public function fetch_place_by_name($name){
+            $query = "SELECT * FROM Digitour WHERE name='" . $name . "'" ;
+            $current_place = mysql_fetch_array($result);
         }
-        return  $current_place["name"] ;
-    }
 
-    function get_current_place_description(){
-        global $current_place ;
-        if($current_place == null){
-            Print '<script>alert("Place not set!");</script>';    
+        public function move($direction){ //use "north", south, east, west
+            switch($direction){
+                case "north":
+                    $current_place == fetch_place_by_id($current_place["adjacent_north_id"]) ;
+                    break;
+                case "south":
+                    $current_place == fetch_place_by_id($current_place["adjacent_south_id"]) ;
+                    break;
+                case "west":
+                    $current_place == fetch_place_by_id($current_place["adjacent_west_id"]) ;
+                    break;
+                case "east":
+                    $current_place == fetch_place_by_id($current_place["adjacent_east_id"]) ;
+                    break;
+                default:
+                    Print '<script>alert("Direction invalid!");</script>'; 
+
+            }
+        } 
+        public function get_current_place_image(){
+            if($current_place == null){
+                Print '<script>alert("Place not set!");</script>';    
+            }
+            return  $current_place["image_path"] ;
         }
-        return  $current_place["description"] ;       
-    }
 
+        public function get_current_place_name(){
+            global $current_place ;
+            if($current_place == null){
+                Print '<script>alert("Place not set!");</script>';    
+            }
+            return  $current_place["name"] ;
+        }
+
+        public function get_current_place_description(){
+            global $current_place ;
+            if($current_place == null){
+                Print '<script>alert("Place not set!");</script>';    
+            }
+            return  $current_place["description"] ;       
+        }
+    }
 ?>
